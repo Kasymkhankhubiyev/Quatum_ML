64c8bfc55123439b1af56720fec7c757c7021b2c
Kaseke <kasymkhankhubievnis@gmail.com>
2022-10-22 18:03:54 +0700

    commit changes

---
+++ Binary_Classification/Normal_distribution/DataPreparation.py
@@ -0,0 +1,44 @@
+"""
+Here I prepare Data for work flow
+"""
+import numpy as np
+from typing import NamedTuple
+
+
+class DataSet(NamedTuple):
+    trainX: list
+    trainY: list
+    testX: list
+    testY: list
+
+
+Distributions = ['Normal']
+
+
+def _create_normal_distributed_data(classes_scale: int, intersect_rate: int) -> DataSet:
+    np.random.seed(0)
+    l = classes_scale
+    n = 2
+    drop = intersect_rate
+
+    X1 = np.array([[-1, -1]]) + drop * np.random.randn(l, n)
+    X2 = np.array([[1, 1]]) + drop * np.random.randn(l, n)
+
+    # конкатенируем все в одну матрицу
+    # при этом по 20 точек оставим на тест/валидацию
+    X = np.vstack((X1[10:], X2[10:]))
+    ValX = np.vstack((X1[:10], X2[:10]))
+
+    # конкатенируем все в один столбец с соответствующими значениями для класса 0 или 1
+    y = np.hstack([[0] * (l - 10), [1] * (l - 10)])
+    ValY = np.hstack([[0] * 10, [1] * 10])
+
+    return DataSet(trainX=X, trainY=y, testX=ValX, testY=ValY)
+
+
+def create_data_set(distribution: str, classes_scale: int, intersect_rate: int) -> DataSet:
+    if distribution in Distributions:
+        if distribution == 'Normal':
+            return _create_normal_distributed_data(classes_scale, intersect_rate)
+    else:
+        print('Unknown distribution')
---
+++ Binary_Classification/Normal_distribution/VisualizeData.py
@@ -0,0 +1,4 @@
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+
