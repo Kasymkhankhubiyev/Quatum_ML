49f60c78708a1d432f936ecb5895dfe4ef83ac9f
Kaseke <kasymkhankhubievnis@gmail.com>
2022-10-28 17:21:33 +0700

    Started a Fisher classification problem

---
+++ Fisher Iris/fisher.py
@@ -0,0 +1,21 @@
+"""
+Multiclass classification with the Fisher Iris dataset
+"""
+import strawberryfields as sf
+from strawberryfields import ops
+import numpy as np
+from qmlt.numerical import CircuitLearner
+from qmlt.numerical.helpers import make_param
+from qmlt.numerical.losses import cross_entropy_with_softmax
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+import seaborn as sns
+import numpy as np
+import pandas as pd
+# import tensorflow as tf
+
+class Model:
+
+    def __init__(self) -> None:
+        self.lr, self.steps = None, None
+        self.params = None
