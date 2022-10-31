45f438b11878251b4266d489ca135c9e5af7cce5
Kaseke <kasymkhankhubievnis@gmail.com>
2022-10-31 15:56:01 +0700

    started creating a decision boundary drawing script

--- Binary_Classification/Normal_distribution/VisualizeData.py
+++ Binary_Classification/Normal_distribution/VisualizeData.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LogisticRegression
+# from sklearn.linear_model import LogisticRegression
+import matplotlib as mpl
 import numpy as np
+# from Circuit import Model
 
 
-def visualize_dataset(trainX, trainY, testX, testY, name: str)-> None:
+def visualize_dataset(trainX, trainY, testX, testY, name: str) -> None:
     colors = ['blue', 'red', 'yellow', 'green']
 
     for k in np.unique(trainY):
@@ -25,4 +27,80 @@ def visualize_data(arrayX, arrayY, name: str) -> None:
     plt.legend(fontsize=7, ncol=1, facecolor='oldlace', edgecolor='r')
     plt.title(name)
     plt.savefig('Binary_Classification/Normal_distribution/' + name + '_transformed_data_set.png')
+    plt.close()
+
+def plot_double_cake_data(X, Y, ax, num_sectors=None):
+    """Plot double cake data and corresponding sectors."""
+    x, y = X.T
+    cmap = mpl.colors.ListedColormap(["#FF0000", "#0000FF"])
+    ax.scatter(x, y, c=Y, cmap=cmap, s=25, marker="s")
+
+    if num_sectors is not None:
+        sector_angle = 360 / num_sectors
+        for i in range(num_sectors):
+            color = ["#FF0000", "#0000FF"][(i % 2)]
+            other_color = ["#FF0000", "#0000FF"][((i + 1) % 2)]
+            ax.add_artist(
+                mpl.patches.Wedge(
+                    (0, 0),
+                    1,
+                    i * sector_angle,
+                    (i + 1) * sector_angle,
+                    lw=0,
+                    color=color,
+                    alpha=0.1,
+                    width=0.5,
+                )
+            )
+            ax.add_artist(
+                mpl.patches.Wedge(
+                    (0, 0),
+                    0.5,
+                    i * sector_angle,
+                    (i + 1) * sector_angle,
+                    lw=0,
+                    color=other_color,
+                    alpha=0.1,
+                )
+            )
+            ax.set_xlim(-1, 1)
+
+    ax.set_ylim(-1, 1)
+    ax.set_aspect("equal")
+    ax.axis("off")
+
+    return ax
+
+def draw_decision_boundary(model, N_gridpoints=14) -> None:
+
+    #разбиваем область на точки
+    _xx, _yy = np.linspace(-4, 4, N_gridpoints), np.linspace(-4, 4, N_gridpoints)
+
+    _zz = np.zeros_like(_xx)
+
+    points = [[_xx[i], _yy[i]] for i in range(len(_xx))]
+
+    for idx in np.ndindex(*_xx.shape):
+        _zz[idx] = model.predict(points)
+
+    # plot_data = {"_xx": _xx, "_yy": _yy, "_zz": _zz}
+    # ax.contourf(
+    #     _xx,
+    #     _yy,
+    #     _zz,
+    #     cmap=mpl.colors.ListedColormap(["#FF0000", "#0000FF"]),
+    #     alpha=0.2,
+    #     levels=[-1, 0, 1],
+    # )
+
+    colors = ['pink', 'purple']
+
+    for k in np.unique(_zz):
+        plt.scatter(x = _xx[_zz == k], y = _yy[_zz == k], s=30, c=colors[k])
+
+    name = 'decision_boundary_test_plot'
+
+    plt.legend(fontsize=7, ncol=1, facecolor='oldlace', edgecolor='r')
+    plt.title(name)
+    plt.savefig('Binary_Classification/Normal_distribution/' + name + '_decision_boundary.png')
     plt.close()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from Binary_Classification.Normal_distribution.DataPreparation import *
 from Binary_Classification.Normal_distribution.VisualizeData import *
-from Binary_Classification import Normal_distribution
+from Binary_Classification.Normal_distribution import Circuit
 
 from FisherIris import DataPrep
 # from FisherIris import VisualizeData
@@ -9,7 +9,7 @@ import FisherIris.fisher
 def run_binary() -> None:
     data = create_data_set(100, 1.5)
     visualize_dataset(data.trainX, data.trainY, data.testX, data.testY, 'Normal 0.5')
-    model = Normal_distribution.Circuit.Model()
+    model = Circuit.Model()
     model.train(lr=1.05, steps=20, trainX=data.trainX, trainY=data.trainY, sq=0.175)
     model.score_model(testX=data.testX, testY=data.testY)
     print('accuracy on train: ')
@@ -17,16 +17,21 @@ def run_binary() -> None:
     print('accuracy on test: ')
     model.predict(data_to_predict=data.testX)
 
+    draw_decision_boundary(model, 20)
 
-if __name__ == '__main__':
-
+def run_fisher() -> None:
     dataset = DataPrep.collect_fisher_dataset()
     model = FisherIris.fisher.Model()
 
-    model.train(lr=.5, steps=200, trainX=dataset.trainX, trainY=dataset.trainY, sq=0.175)
+    model.train(lr=.55, steps=250, trainX=dataset.trainX, trainY=dataset.trainY, sq=0.175)
     model.score_model(testX=dataset.testX, testY=dataset.testY)
 
     print('accuracy on train: ')
     model.predict(data_to_predict=dataset.trainX)
     print('accuracy on test: ')
     model.predict(data_to_predict=dataset.testX)
+
+
+if __name__ == '__main__':
+
+    run_binary()
