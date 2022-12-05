f4230f30a6e5553e11429a8ff3f915840b3a67a9
Kaseke <kasymkhankhubievnis@gmail.com>
2022-12-05 18:18:43 +0700

    commit changes

--- Mnist/CNN/Model.py
+++ Mnist/CNN/Model.py
@@ -33,7 +33,7 @@ class Model:
         elif len(x) == 4:
             return 2, np.array(x.reshape([2, 2]))
 
-    def _layer_circuit(self, x, params):
+    def _layer_circuit(self, x, params, disp):
         sq = self.squeeze_rate
         qnn = sf.Program(4)
 
@@ -71,7 +71,7 @@ class Model:
             ops.Pgate(params[38]) | q[0]
             ops.Pgate(params[39]) | q[1]
             ops.Pgate(params[40]) | q[2]
-            # ops.Pgate(params[41]) | q[3]
+            ops.Pgate(params[41]) | q[3]
             # ops.Kgate(params[42]) | q[0]
             # ops.Kgate(params[43]) | q[1]
             # ops.Kgate(params[44]) | q[2]
@@ -86,7 +86,7 @@ class Model:
 
         return q[2]
 
-    def _layer(self, x, params):
+    def _layer(self, x, params, disp):
         """
         8X8 - 64 // 4 = 16 блоков
         :param x: a single picture
@@ -107,8 +107,67 @@ class Model:
 
         return q
 
-    def _output_layer(self, x, params):
-        pass
+    def _output_layer(self, x, params, disp):
+
+        sq = self.squeeze_rate
+        qnn = sf.Program(4)
+
+        with qnn.context as q:
+            ops.Sgate(sq, x[0]) | q[0]
+            ops.Sgate(sq, x[1]) | q[1]
+            ops.Sgate(sq, x[2]) | q[2]
+            ops.Sgate(sq, x[3]) | q[3]
+            ops.BSgate(params[0], params[1]) | (q[0], q[1])
+            ops.BSgate(params[2], params[3]) | (q[2], q[3])
+            ops.BSgate(params[4], params[5]) | (q[1], q[2])
+            ops.BSgate(params[6], params[7]) | (q[0], q[3])
+            ops.BSgate(params[9], params[8]) | (q[2], q[0])
+            ops.BSgate(params[10], params[11]) | (q[1], q[3])
+            ops.Rgate(params[12]) | q[0]
+            ops.Rgate(params[13]) | q[1]
+            ops.Rgate(params[14]) | q[2]
+            ops.Sgate(params[15]) | q[0]
+            ops.Sgate(params[16]) | q[1]
+            ops.Sgate(params[17]) | q[2]
+            ops.Sgate(params[18]) | q[3]
+            ops.BSgate(params[19], params[20]) | (q[0], q[1])
+            ops.BSgate(params[21], params[22]) | (q[2], q[3])
+            ops.BSgate(params[23], params[24]) | (q[1], q[2])
+            ops.BSgate(params[25], params[26]) | (q[0], q[1])
+            ops.BSgate(params[27], params[28]) | (q[2], q[3])
+            ops.BSgate(params[29], params[30]) | (q[1], q[2])
+            ops.Rgate(params[31]) | q[0]
+            ops.Rgate(params[32]) | q[1]
+            ops.Rgate(params[33]) | q[2]
+            ops.Dgate(params[34]) | q[0]
+            ops.Dgate(params[35]) | q[1]
+            ops.Dgate(params[36]) | q[2]
+            ops.Dgate(params[37]) | q[3]
+            ops.Pgate(params[38]) | q[0]
+            ops.Pgate(params[39]) | q[1]
+            ops.Pgate(params[40]) | q[2]
+            ops.Pgate(params[41]) | q[3]
+
+        eng = sf.Engine('fock', backend_options={'cutoff_dim': 5, 'eval': True})
+
+        result = eng.run(qnn)
+        state = result.state
+
+        ei = [0, 0, 0, 0]  # modes*[0]
+        ei[0] = 2
+        p0 = state.fock_prob(ei)
+        ei[0] = 0
+        ei[1] = 2
+        p1 = state.fock_prob(ei)
+        ei[1] = 0
+        ei[2] = 2
+        p2 = state.fock_prob(ei)
+        ei[2] = 0
+
+        normalization = p0 + p1 + p2 + 1e-10
+        output = [p0 / normalization, p1 / normalization, p2 / normalization]
+
+        return output
 
     # надо возвращать сам регистр
     def _circuit(self, x):
@@ -116,9 +175,14 @@ class Model:
 
         def _single_circuit(x):
 
-            q = self._layer(x, self.params_layer0)
-            q = self._layer(q, self.params_layer1)
-            q = self._layer(q, self.params_layer1)
+            q = self._layer(x, self.params_layer0, disp)
+            print(f'layer 0: {q}')
+            q = self._layer(q, self.params_layer1, disp)
+            print(f'layer 1: {q}')
+            # q = self._layer(q, self.params_layer2)
+            # print(f'layer 0: {q}')
+
+            return self._output_layer(q, self.params_layer2, disp)
 
 
         pass
