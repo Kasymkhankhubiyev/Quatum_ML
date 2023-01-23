60b3d930d26c3e7e8d8ebaa93463759e7f4bd933
Kaseke <kasymkhankhubievnis@gmail.com>
2023-01-23 11:15:25 +0700

    changed the way the model uploads its params

--- Binary_Classification/Normal_distribution/Circuit.py
+++ Binary_Classification/Normal_distribution/Circuit.py
@@ -5,6 +5,7 @@ from qmlt.numerical.helpers import make_param
 from qmlt.numerical.losses import square_loss
 import datetime
 from Binary_Classification.Normal_distribution.VisualizeData import *
+from helper import upload_params
 
 squeeze = float
 
@@ -18,19 +19,25 @@ class Model:
         self.counter = 0
 
     def _circuit(self, X, params):
-        # print(self.counter)
-        # self.counter += 1
-        # prog = sf.Program(2)
         sq = self.squeeze_param
 
-        # transform_data = []
+        def simple_run(x):
+            prog = sf.Program(2)
+
+            with prog.context as q:
+                ops.Sgate(sq, x[0]) | q[0]
+                ops.Sgate(sq, x[1]) | q[1]
+                ops.BSgate(params[0], params[7]) | (q[0], q[1])
+                ops.MeasureFock() | q[0]
+
+            eng = sf.Engine('fock', backend_options={'cutoff_dim': 5, 'eval': True})
+            eng.run(prog)
+            n = q[0].val
 
         def single_input_circuit(x):
             prog = sf.Program(2)
 
             with prog.context as q:
-                # ops.Dgate(x[0], 0.) | q[0]
-                # ops.Dgate(x[1], 0.) | q[1]
                 ops.Sgate(sq, x[0]) | q[0]
                 ops.Sgate(sq, x[1]) | q[1]
                 ops.BSgate(params[0], params[7]) | (q[0], q[1])
@@ -40,9 +47,6 @@ class Model:
                 ops.Pgate(params[4]) | q[1]
                 ops.Kgate(params[5]) | q[0]
                 ops.Kgate(params[6]) | q[1]
-
-                # ops.MeasureFock() | q[0]
-                # ops.MeasureFock() | q[1]
 
             eng = sf.Engine('fock', backend_options={'cutoff_dim': 5, 'eval': True})
 
@@ -57,7 +61,12 @@ class Model:
 
             return output
 
-        circuit_output = [single_input_circuit(x) for x in X]
+        circuit_output = []
+        for x in X:
+            simple_run(x)
+            circuit_output.append(single_input_circuit(x))
+
+        # circuit_output = [single_input_circuit(x) for x in X]
 
         return circuit_output
 
@@ -66,14 +75,6 @@ class Model:
 
     def _outputs_to_predictions(self, circuit_output):
         return round(circuit_output)
-
-    def _upload_params(self):
-        name = 'Binary_Classification/Normal_distribution/params_on_'+ \
-            (datetime.datetime.strftime(datetime.datetime.now(), "%Y_%m_%d"))+'.txt'
-        with open(name, 'a') as file:
-            for i in range(len(self.params)):
-                file.write(str(self.params[i])+',')
-                file.write('\n\n\n')
 
     def train(self, lr: float, steps: int, sq: squeeze, trainX, trainY) -> None:
         self.squeeze_param = sq
@@ -91,7 +92,8 @@ class Model:
 
         self.learner = CircuitLearner(hyperparams=hyperparams)
         self.learner.train_circuit(X=trainX, Y=trainY, steps=steps)
-        self._upload_params()
+        upload_params(file_name='Binary_Classification/Normal_distribution/params.txt',
+                      input_name='BinarClf_params', data=self.params)
 
     def predict(self, data_to_predict):
         outcomes = self.learner.run_circuit(X=data_to_predict, outputs_to_predictions=self._outputs_to_predictions)
@@ -114,29 +116,3 @@ class Model:
                 file.write('x: '+str(testX[i]) + ', y: '+str(testY[i]) + '\n')
             file.write("Accuracy on test set: {}".format(test_score['accuracy']) + '\n')
             file.write("Loss on test set: {}".format(test_score['loss']) + '\n\n\n')
-
-    def _project_data(self, data):
-        sq = self.squeeze_param
-
-        transform_data = []
-
-        for x in data:
-            prog = sf.Program(2)
-
-            with prog.context as q:
-                # ops.Dgate(x[0], 0.) | q[0]
-                # ops.Dgate(x[1], 0.) | q[1]
-                ops.Sgate(sq, x[0]) | q[0]
-                ops.Sgate(sq, x[1]) | q[1]
-                ops.BSgate(self.params[0]['val'], self.params[7]['val']) | (q[0], q[1])
-                ops.Dgate(self.params[1]['val']) | q[0]
-                ops.Dgate(self.params[2]['val']) | q[1]
-                ops.Pgate(self.params[3]['val']) | q[0]
-                ops.Pgate(self.params[4]['val']) | q[1]
-                ops.Kgate(self.params[5]['val']) | q[0]
-                ops.Kgate(self.params[6]['val']) | q[1]
-
-            eng = sf.Engine('fock', backend_options={'cutoff_dim': 5, 'eval': True})
-            eng.run(prog)
-
-        return np.array(transform_data)
