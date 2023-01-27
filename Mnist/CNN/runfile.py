7d76b69962baeb273e4c09088e4a1ef0b02e1a42
Kaseke <kasymkhankhubievnis@gmail.com>
2023-01-27 23:25:47 +0700

    testing model

--- Mnist/CNN/runfile.py
+++ Mnist/CNN/runfile.py
@@ -2,6 +2,7 @@ from Mnist.CNN.DataPrep import create_dataset, create_dataset_binary
 from Mnist.CNN.VisualizeData import draw_samples
 from Mnist.CNN.Model import Model
 
+
 def run() -> None:
     digits = create_dataset_binary(class0=1, class1=7)
     # draw_samples(dataset=digits, samples_number=8)
@@ -9,6 +10,6 @@ def run() -> None:
     print(digits.testY)
     model = Model()
     print(f'trainX = {digits.trainX.shape}')
-    model.fit(lr=1., sq=1.5, steps=5, clf_task='binary', trainX=digits.trainX, trainY=digits.trainY)
+    model.fit(lr=.75, sq=1.5, steps=10, clf_task='binary', trainX=digits.trainX, trainY=digits.trainY)
     model.predict(data_to_predict=digits.testX)
     model.score_model(testX=digits.testX, testY=digits.testY)
