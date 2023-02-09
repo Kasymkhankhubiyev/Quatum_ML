2c319873f86d6136f126332866077b4feaabe286
Kaseke <kasymkhankhubievnis@gmail.com>
2023-02-09 20:46:04 +0700

    commit tests results

--- Mnist/CNN/runfile.py
+++ Mnist/CNN/runfile.py
@@ -6,6 +6,6 @@ def run() -> None:
     digits = create_dataset_binary(class0=1, class1=4)
     model = Model()
     print(f'trainX = {digits.trainX.shape}')
-    model.fit(lr=.175, sq=1.5, steps=15, clf_task='binary', train_x=digits.trainX, train_y=digits.trainY)
+    model.fit(lr=.35, sq=1.5, steps=10, clf_task='binary', train_x=digits.trainX, train_y=digits.trainY)
     model.predict(data_to_predict=digits.testX)
     model.score_model(test_x=digits.testX, test_y=digits.testY)
